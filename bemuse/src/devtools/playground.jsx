19af9ca75459e1e1d2ca06e1655baf0e26e16aad
MikuroXina <10331164+MikuroXina@users.noreply.github.com>
2026-02-21 04:52:35 +0900

    Avoid require

--- bemuse/src/devtools/playground.jsx
+++ bemuse/src/devtools/playground.jsx
@@ -1,41 +1,38 @@
 import { sceneRoot } from '@bemuse/utils/main-element.js'
 import query from '@bemuse/utils/query.js'
-import React from 'react'
 
-const availablePlaygrounds = (function (context) {
+const availablePlaygrounds = await (async function (context) {
   const playgrounds = {}
-  for (const key of context.keys()) {
+  for (const key of Object.keys(context)) {
     const name = key.match(/\w[^.]+/)[0]
-    playgrounds[name] = context(key)
+    playgrounds[name] = await context[key]()
   }
   return playgrounds
-})(require.context('./playgrounds', false, /\.[jt]sx?$/))
+})(import.meta.glob('./playgrounds*{j,t}s{x,}.'))
 
-class DefaultPlayground extends React.Component {
-  static main() {
-    sceneRoot.render(<DefaultPlayground />)
-  }
-
-  render() {
-    const linkStyle = { color: '#abc' }
-    return (
-      <div>
-        <h1>Bemuse Playground</h1>
-        <p>Please select a playground</p>
-        <ul>
-          {Object.keys(availablePlaygrounds).map((key) => (
-            <li key={key}>
-              <a style={linkStyle} href={'?mode=playground&playground=' + key}>
-                {key}
-              </a>
-            </li>
-          ))}
-        </ul>
-      </div>
-    )
-  }
+const DefaultPlayground = () => {
+  const linkStyle = { color: '#abc' }
+  return (
+    <div>
+      <h1>Bemuse Playground</h1>
+      <p>Please select a playground</p>
+      <ul>
+        {Object.keys(availablePlaygrounds).map((key) => (
+          <li key={key}>
+            <a style={linkStyle} href={'?mode=playground&playground=' + key}>
+              {key}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
 }
 
 export function main() {
-  ;(availablePlaygrounds[query.playground] || DefaultPlayground).main()
+  if (availablePlaygrounds[query.playground]) {
+    availablePlaygrounds[query.playground].main()
+  } else {
+    sceneRoot.render(<DefaultPlayground />)
+  }
 }
