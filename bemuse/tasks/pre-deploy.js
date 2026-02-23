cab4035d601d2dd64e4e5aa89c38814caffb5aaf
MikuroXina <10331164+MikuroXina@users.noreply.github.com>
2026-02-24 02:00:15 +0900

    Enforce pre-deploy script

--- bemuse/tasks/pre-deploy.js
+++ bemuse/tasks/pre-deploy.js
@@ -3,6 +3,10 @@ import { promises } from 'node:fs'
 import gulp from 'gulp'
 
 gulp.task('pre-deploy', async () => {
+  await promises.cp('../website/build/', 'dist/project/', {
+    recursive: true,
+  })
+
   const data = await promises.readFile('dist/index.html', 'utf-8')
   check('Index file size is less than 200 KB', () => data.length < 200e3)
 
