0973a6daed1044f3eaa8199d20ff21feeb89c45c
MikuroXina <10331164+MikuroXina@users.noreply.github.com>
2026-02-25 06:02:03 +0900

    Ignore lint dev-dist

--- packages/bemuse/eslint.config.mjs
+++ packages/bemuse/eslint.config.mjs
@@ -3,6 +3,9 @@ import bemuseConfig from '@mikuroxina/eslint-config-bemuse'
 export default [
   ...bemuseConfig,
   {
+    ignores: ['dev-dist/'],
+  },
+  {
     rules: {
       'n/no-unsupported-features/node-builtins': 'off',
       'n/no-missing-import': 'off',
