3730410461b0a69cf35bd583ff5187f422184301
MikuroXina <10331164+MikuroXina@users.noreply.github.com>
2026-03-03 14:50:21 +0900

    Fix release glitch

--- packages/bemuse/src/game/input/omni-input-plugin.js
+++ packages/bemuse/src/game/input/omni-input-plugin.js
@@ -22,21 +22,23 @@ function OmniInputPlugin(game) {
       const data = input.update()
 
       const result = {
-        p1_1: data[kbm['1'] ?? 'KeyS'],
-        p1_2: data[kbm['2'] ?? 'KeyD'],
-        p1_3: data[kbm['3'] ?? 'KeyF'],
-        p1_4: data[kbm['4'] ?? 'Space'],
-        p1_5: data[kbm['5'] ?? 'KeyJ'],
-        p1_6: data[kbm['6'] ?? 'KeyK'],
-        p1_7: data[kbm['7'] ?? 'KeyL'],
+        p1_1: data[kbm['1'] ?? 'KeyS'] ? 1 : 0,
+        p1_2: data[kbm['2'] ?? 'KeyD'] ? 1 : 0,
+        p1_3: data[kbm['3'] ?? 'KeyF'] ? 1 : 0,
+        p1_4: data[kbm['4'] ?? 'Space'] ? 1 : 0,
+        p1_5: data[kbm['5'] ?? 'KeyJ'] ? 1 : 0,
+        p1_6: data[kbm['6'] ?? 'KeyK'] ? 1 : 0,
+        p1_7: data[kbm['7'] ?? 'KeyL'] ? 1 : 0,
         p1_SC: scratch.combine(
           data[kbm['SC'] || 'KeyA'],
           data[kbm['SC2'] || 'ShiftLeft']
         ),
-        p1_speedup: data['ArrowUp'],
-        p1_speeddown: data['ArrowDown'],
-        start: data['Enter'] || (isBtn9Free && data['gamepad.0.button.9']),
-        select: data['AltLeft'] || (isBtn8Free && data['gamepad.0.button.8']),
+        p1_speedup: data['ArrowUp'] ? 1 : 0,
+        p1_speeddown: data['ArrowDown'] ? 1 : 0,
+        start:
+          data['Enter'] ?? ((isBtn9Free && data['gamepad.0.button.9']) || 0),
+        select:
+          data['AltLeft'] ?? ((isBtn8Free && data['gamepad.0.button.8']) || 0),
       }
       if (result['start'] || result['select']) {
         if (
@@ -45,10 +47,10 @@ function OmniInputPlugin(game) {
           result['p1_5'] ||
           result['p1_7']
         ) {
-          result['p1_speeddown'] = true
+          result['p1_speeddown'] = 1
         }
         if (result['p1_2'] || result['p1_4'] || result['p1_6']) {
-          result['p1_speedup'] = true
+          result['p1_speedup'] = 1
         }
       }
       return result
