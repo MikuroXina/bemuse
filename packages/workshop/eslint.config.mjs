0fbdaca5ae1c4e7bee6b3760bfe9292b799671a0
MikuroXina <10331164+MikuroXina@users.noreply.github.com>
2026-03-06 09:11:39 +0900

    Setup lint

--- packages/workshop/eslint.config.mjs
+++ packages/workshop/eslint.config.mjs
@@ -3,7 +3,7 @@ import bemuseConfig from '@mikuroxina/eslint-config-bemuse'
 export default [
   ...bemuseConfig,
   {
-    ignores: ['build/'],
+    ignores: ['build/', '.react-router/'],
   },
   {
     rules: {
