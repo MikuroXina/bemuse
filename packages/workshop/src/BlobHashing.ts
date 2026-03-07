fc83841c4f827500e95362000dde4844496b6e2b
MikuroXina <10331164+MikuroXina@users.noreply.github.com>
2026-03-07 09:30:48 +0900

    Remove old files

--- packages/workshop/src/BlobHashing.ts
+++
@@ -1,8 +0,0 @@
-import CryptoJS from "crypto-js";
-
-export async function hashBlob(blob: Blob) {
-  var wordArray = CryptoJS.lib.WordArray.create(
-    (await blob.arrayBuffer()) as any
-  );
-  return CryptoJS.MD5(wordArray).toString();
-}
