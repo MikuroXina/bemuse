2298ecee517fa3e6c1dab5f133f5d5571796fd48
MikuroXina <10331164+MikuroXina@users.noreply.github.com>
2026-03-14 20:55:18 +0900

    Change types design

--- packages/scoreboard-types/src/auth.ts
+++ packages/scoreboard-types/src/auth.ts
@@ -15,30 +15,6 @@ export const userInfoSchema = v.object({
 })
 export type UserInfo = v.InferOutput<typeof userInfoSchema>
 
-export const signUpParameterSchema = v.object({
-  state: v.string(),
-})
-
-export const signUpCallbackParameterSchema = v.object({
-  /** OAuth 2.0 authorization code. */
-  code: v.string(),
-  state: v.string(),
-})
-export const signUpCallbackResponseSchema = v.object({
-  user_info: userInfoSchema,
-  /** A Bearer token which you can use on other APIs of this service to authorize. */
-  access_token: v.string(),
-  /** A Bearer token which you can pass to `/refresh` when the access token has expired. */
-  refresh_token: v.string(),
-})
-
-export const refreshResponseSchema = v.object({
-  /** A Bearer token which you can use on other APIs of this service to authorize. */
-  access_token: v.string(),
-  /** A Bearer token which you can pass to `/refresh` when the access token has expired. */
-  refresh_token: v.string(),
-})
-
 export const updateUserRequestSchema = v.object({
   name: v.pipe(v.string(), v.minLength(1)),
 })
