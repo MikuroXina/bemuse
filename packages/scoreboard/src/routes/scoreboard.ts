aa556d01248088d9aa2e5c5587b3e96ba74c88c8
MikuroXina <10331164+MikuroXina@users.noreply.github.com>
2026-03-28 00:47:09 +0900

    Fix scoreboard routes errors

--- packages/scoreboard/src/routes/scoreboard.ts
+++ packages/scoreboard/src/routes/scoreboard.ts
@@ -3,7 +3,7 @@ import { Scoreboard } from '@mikuroxina/scoreboard-types'
 import { Hono } from 'hono'
 import { env } from 'hono/adapter'
 
-import { idProvider } from '../adaptor/auth0'
+import { idProvider, userRepo } from '../adaptor/auth0'
 import type { Bindings, EnvVars } from '../env'
 import { authMiddleware } from '../middleware'
 import { getLeaderboard, getScore, submitScore } from '../service/scoreboard'
@@ -15,28 +15,42 @@ export const router = new Hono<{ Bindings: Bindings }>().basePath(
 router.get(
   '/:chart_md5/:play_mode',
   sValidator('param', Scoreboard.getLeaderboardParameterSchema),
-  async (c) =>
-    c.json(
+  async (c) => {
+    const { VITE_AUTH0_DOMAIN, VITE_AUTH0_CLIENT_ID, AUTH0_CLIENT_SECRET } =
+      env<EnvVars>(c)
+    return c.json(
       await getLeaderboard({
         param: c.req.valid('param'),
         score: c.env.score,
+        userRepo: userRepo({
+          auth0Domain: VITE_AUTH0_DOMAIN,
+          auth0ClientId: VITE_AUTH0_CLIENT_ID,
+          auth0ClientSecret: AUTH0_CLIENT_SECRET,
+        }),
       })
     )
+  }
 )
 
 router.post(
-  '/:chart_id/:play_mode',
+  '/:chart_md5/:play_mode',
   authMiddleware,
   sValidator('param', Scoreboard.submitScoreParameterSchema),
   sValidator('json', Scoreboard.submitScoreRequestBodySchema),
   async (c) => {
-    const { VITE_AUTH0_DOMAIN } = env<EnvVars>(c)
+    const { VITE_AUTH0_DOMAIN, VITE_AUTH0_CLIENT_ID, AUTH0_CLIENT_SECRET } =
+      env<EnvVars>(c)
     return c.json(
       await submitScore({
         accessToken: c.get('accessToken'),
         param: c.req.valid('param'),
         toSubmit: c.req.valid('json'),
         idp: idProvider(VITE_AUTH0_DOMAIN),
+        userRepo: userRepo({
+          auth0Domain: VITE_AUTH0_DOMAIN,
+          auth0ClientId: VITE_AUTH0_CLIENT_ID,
+          auth0ClientSecret: AUTH0_CLIENT_SECRET,
+        }),
         db: c.env.score,
       })
     )
@@ -44,13 +58,21 @@ router.post(
 )
 
 router.get(
-  '/:chart_id/:play_mode/:user_id',
+  '/:chart_md5/:play_mode/:user_id',
   sValidator('param', Scoreboard.getScoreParameterSchema),
-  async (c) =>
-    c.json(
+  async (c) => {
+    const { VITE_AUTH0_DOMAIN, VITE_AUTH0_CLIENT_ID, AUTH0_CLIENT_SECRET } =
+      env<EnvVars>(c)
+    return c.json(
       await getScore({
         param: c.req.valid('param'),
         score: c.env.score,
+        userRepo: userRepo({
+          auth0Domain: VITE_AUTH0_DOMAIN,
+          auth0ClientId: VITE_AUTH0_CLIENT_ID,
+          auth0ClientSecret: AUTH0_CLIENT_SECRET,
+        }),
       })
     )
+  }
 )
