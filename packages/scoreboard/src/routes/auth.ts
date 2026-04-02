8c421deb01864f392da8c4c2dbb76f1b43d7ec46
MikuroXina <10331164+MikuroXina@users.noreply.github.com>
2026-04-03 01:53:39 +0900

    Enable cache

--- packages/scoreboard/src/routes/auth.ts
+++ packages/scoreboard/src/routes/auth.ts
@@ -3,6 +3,7 @@ import { Auth } from '@mikuroxina/scoreboard-types'
 import { ManagementClient, UserInfoClient } from 'auth0'
 import { Hono } from 'hono'
 import { env } from 'hono/adapter'
+import { cache } from 'hono/cache'
 import type { InferOutput } from 'valibot'
 
 import type { Env, Variables } from '../env'
@@ -14,34 +15,41 @@ router.use(corsMiddleware)
 router.use(authMiddleware)
 
 router
-  .get('/users/me', async (c) => {
-    const { VITE_AUTH0_DOMAIN, VITE_AUTH0_CLIENT_ID, AUTH0_CLIENT_SECRET } =
-      env<Variables>(c)
+  .get(
+    '/users/me',
+    cache({
+      cacheName: 'user-info',
+      cacheControl: 'max-age=86400',
+    }),
+    async (c) => {
+      const { VITE_AUTH0_DOMAIN, VITE_AUTH0_CLIENT_ID, AUTH0_CLIENT_SECRET } =
+        env<Variables>(c)
 
-    const user = await new UserInfoClient({
-      domain: VITE_AUTH0_DOMAIN,
-    }).getUserInfo(c.get('accessToken'))
-    if (user.status !== 200) {
-      console.error(user.data)
-      return c.text('Unauthorized', 401)
+      const user = await new UserInfoClient({
+        domain: VITE_AUTH0_DOMAIN,
+      }).getUserInfo(c.get('accessToken'))
+      if (user.status !== 200) {
+        console.error(user.data)
+        return c.text('Unauthorized', 401)
+      }
+
+      const userId = user.data.sub
+
+      const management = new ManagementClient({
+        domain: VITE_AUTH0_DOMAIN,
+        clientId: VITE_AUTH0_CLIENT_ID,
+        clientSecret: AUTH0_CLIENT_SECRET,
+      })
+      const res = await management.users.get(userId)
+      const ret: InferOutput<typeof Auth.userInfoSchema> = {
+        id: res.user_id! as Auth.UserId,
+        name: res.name!,
+        created_at: res.created_at! as string,
+        is_frozen: res.blocked ?? false,
+      }
+      return c.json(ret)
     }
-
-    const userId = user.data.sub
-
-    const management = new ManagementClient({
-      domain: VITE_AUTH0_DOMAIN,
-      clientId: VITE_AUTH0_CLIENT_ID,
-      clientSecret: AUTH0_CLIENT_SECRET,
-    })
-    const res = await management.users.get(userId)
-    const ret: InferOutput<typeof Auth.userInfoSchema> = {
-      id: res.user_id! as Auth.UserId,
-      name: res.name!,
-      created_at: res.created_at! as string,
-      is_frozen: res.blocked ?? false,
-    }
-    return c.json(ret)
-  })
+  )
   .post(sValidator('param', Auth.updateUserRequestSchema), async (c) => {
     const { VITE_AUTH0_DOMAIN, VITE_AUTH0_CLIENT_ID, AUTH0_CLIENT_SECRET } =
       env<Variables>(c)
