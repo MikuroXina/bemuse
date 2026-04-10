ab0189d8896deefe2f4357332734f02060bd41a0
MikuroXina <10331164+MikuroXina@users.noreply.github.com>
2026-04-11 05:15:57 +0900

    Setup vitest integrated d1 test

---
+++ packages/scoreboard/test/apply-migrations.ts
@@ -0,0 +1,7 @@
+import { applyD1Migrations } from 'cloudflare:test'
+import { env } from 'cloudflare:workers'
+
+// Setup files run outside isolated storage, and may be run multiple times.
+// `applyD1Migrations()` only applies migrations that haven't already been
+// applied, therefore it is safe to call this function here.
+await applyD1Migrations(env.score, env.TEST_MIGRATIONS)
--- packages/scoreboard/vite.config.ts
+++ packages/scoreboard/vite.config.ts
@@ -1,12 +1,35 @@
-import { cloudflare } from '@cloudflare/vite-plugin'
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import path from 'node:path'
 
-export default defineConfig({
-  plugins: [react(), cloudflare()],
-  server: {
-    // delegated to Hono's CORS middleware
-    cors: false,
-    port: 8787,
-  },
+import { cloudflare } from '@cloudflare/vite-plugin'
+import {
+  cloudflareTest,
+  readD1Migrations,
+} from '@cloudflare/vitest-pool-workers'
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig(async () => {
+  const migrationsPath = path.join(__dirname, 'migrations')
+  const migrations = await readD1Migrations(migrationsPath)
+
+  return {
+    plugins: [
+      react(),
+      cloudflare(),
+      cloudflareTest({
+        miniflare: {
+          bindings: { TEST_MIGRATIONS: migrations },
+        },
+      }),
+    ],
+    server: {
+      // delegated to Hono's CORS middleware
+      cors: false,
+      port: 8787,
+    },
+    test: {
+      exclude: ['node_modules/', 'dist/'],
+      setupFiles: ['./test/apply-migrations.ts'],
+    },
+  }
 })
